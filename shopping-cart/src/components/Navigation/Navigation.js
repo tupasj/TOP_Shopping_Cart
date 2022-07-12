240253cda64a9234a023deefef2477a13633ed2e
Jeremy Tupas <jeremytupas@gmail.com>
2022-07-11 21:15:38 -0500

    Added static nav

--- shopping-cart/src/components/Navigation/Navigation.js
+++ shopping-cart/src/components/Navigation/Navigation.js
@@ -1,7 +1,11 @@
 const Navigation = () => {
     return (
-        <>
-        </>
+        <nav>
+            <a href="#">Category</a>
+            <a href="#">Brand</a>
+            <a href="#">Price</a>
+            <a href="#" className="nav__anchor--red">Special offers</a>
+        </nav>
     )
 }
 
