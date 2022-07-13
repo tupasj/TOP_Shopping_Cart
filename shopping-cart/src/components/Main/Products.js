563bd6b6272f265b2eb231536f70f925a6a1795f
Jeremy Tupas <jeremytupas@gmail.com>
2022-07-13 12:46:35 -0500

    Added first product card

--- shopping-cart/src/components/Main/Products.js
+++ shopping-cart/src/components/Main/Products.js
@@ -1,13 +1,17 @@
-import MensClothing from '../../assets/data/MensClothing.json';
+import MensClothing from "../../assets/data/MensClothing.json";
 
 const Products = () => {
   return (
     <div className="products">
       <div className="product">
-        <img className="product__image" src={MensClothing.MensClothing[0].image} alt={MensClothing.MensClothing[0].alt}></img>
-        <div className="product__name">{MensClothing.MensClothing[0].name}</div>
-        <div className="product__reviews">Product reviews</div>
-        <div className="product__price">Product price</div>
+        <img
+          className="product__image"
+          src={MensClothing.Set1[0].src}
+          alt={MensClothing.Set1[0].alt}
+        ></img>
+        <div className="product__name">{MensClothing.Set1[0].name}</div>
+        <div className="product__rating">{MensClothing.Set1[0].rating}</div>
+        <div className="product__price">${MensClothing.Set1[0].price}</div>
         <div className="product__buttons">
           <button>View</button>
           <button>Add to wishlist</button>
