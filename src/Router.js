09726a72a8b45ddb28c83b8e7220d027646cc451
Jeremy Tupas <jeremytupas@gmail.com>
2022-07-22 18:10:41 -0500

    Switch BrowserRouter to HashRouter

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { App } from "./App";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/*" element={<App />} />
         <Route path="/" element={<Navigate replace to="/men" />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
