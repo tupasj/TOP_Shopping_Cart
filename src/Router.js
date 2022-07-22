2726c94e02e9a01708587e6ce8b28a8df9949767
Jeremy Tupas <jeremytupas@gmail.com>
2022-07-22 18:53:42 -0500

    Added baseName to BrowserRouter

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,14 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { App } from "./App";
 
 const Router = () => {
   return (
-    <HashRouter>
+    <BrowserRouter baseName={'//tupasj.github.io/TOP_Shopping_Cart'}>
       <Routes>
         <Route path="/*" element={<App />} />
         <Route path="/" element={<Navigate replace to="/men" />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
