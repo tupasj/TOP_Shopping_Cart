130b864db7b615d4d3cbf42dc82a9d243225baa3
Jeremy Tupas <jeremytupas@gmail.com>
2022-08-05 20:49:06 -0500

    Moved BrowserRouter to App component and changed baseName.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
 import { useState } from "react";
 import { Header } from "./components/Header";
 import { Navigation } from "./components/Navigation";
 import { Main } from "./components/Main";
 
 const App = () => {
-  const [ itemCount, setItemCount ] = useState(0);
+  const [itemCount, setItemCount] = useState(0);
 
   return (
-    <>
+    <BrowserRouter baseName={'/TOP_Shopping_Cart'}>
       <Header itemCount={itemCount} />
       <Navigation />
       <Main itemCount={itemCount} setItemCount={setItemCount} />
-    </>
+    </BrowserRouter>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/styles.css";
-import { Router } from "./Router";
+// import { Router } from "./Router";
+import { App } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router />
+    {/* <Router /> */}
+    <App />
   </React.StrictMode>
 );
